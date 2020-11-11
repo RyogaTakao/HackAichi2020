1a471457ba9947d244d3815aa1126721f5106382
TatsuyaOkazaki <180442029@ccmailg.meijo-u.ac.jp>
2020-11-11 22:02:22 +0900

    いろんな条件を追加したよ

--- src/StartUpCmand.py
+++ src/StartUpCmand.py
@@ -3,13 +3,16 @@ import RPi.GPIO as GPIO # RPi.GPIOモジュールを使用
 import time # timeライブラリ
 
 # GPIOの使用するピン番号
-gpio_sw = 40
+gpio_sw = 21
 # GPIO番号指定の準備
 GPIO.setmode(GPIO.BCM)
 # スイッチピンを入力、プルアップに設定
 GPIO.setup(gpio_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIOの状態に変化が生じたことを確認する変数
-gpio_after = 0
+
+clock_prop = object
+video_prop = object
+
+before_sw = 1
 
 # コマンド実行関数
 # async :非期処理 sync :同期処理
@@ -27,34 +30,45 @@ def run_cmd_func (cmd , status) :
 # 実行フローはここから========================================
 
 # Node-REDの起動コマンドを実行（同期処理）
-nodered_start_cmd = 'node-red-pi --max-old-space-size=256'
-run_cmd_func(nodered_start_cmd, 'sync')
+# nodered_start_cmd = 'node-red-pi --max-old-space-size=256'
+# run_cmd_func(nodered_start_cmd, 'sync')
 
-# スタート画面（時計）をkioskモードで表示するコマンドを実行
-chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/clock'
+# 背景をkioskモードで表示するコマンドを実行
+chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/home'
 prop_clock = run_cmd_func(chromium_start_cmd, 'async')
 
 # 永久ループでGPIOをの状態をチェックする
 while True :
     # スイッチの状態を取得
     sw = GPIO.input(gpio_sw)
-    # スイッチの状態に変化が存在していれば処理を開始
-    if (gpio_after != sw) :
-        # スイッチが押されていたら（ON = 0）
-        if sw == 0 :
-            # コマンドラインでSkyWayを起動するページを表示する処理を追加
-            chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/videochat'
-            prop_skyway = run_cmd_func(chromium_start_cmd, 'async')
-            # clockを消す
-            prop_clock.kill()
-            #gpioの最終状態を保存
-            gpio_after = sw
-        # スイッチが押されていなかったら
-        else :
-            # コマンドラインで時計ページを表示する処理を追加
-            chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/clock'
-            prop_skyway = run_cmd_func(chromium_start_cmd, 'async')
-            # clockを消す
-            prop_clock.kill()
-            #gpioの最終状態を保存
-            gpio_after = sw
+    if before_sw != sw :
+        if sw == 0 : # ものが置かれている
+            time.sleep(5)
+            sw = GPIO.input(gpio_sw)
+            if sw == 0 : # まだものが置かれているなら 背景にする
+                if clock_prop : # 時計が表示されていたら消す
+                    clock_prop.kill()
+                    clock_prop = ''
+                if video_prop : # ビデオが表示されていたら消す
+                    video_prop.kill()
+                    clock_prop = ''
+            else : 
+                chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/videochat'
+                video_prop = run_cmd_func(chromium_start_cmd, 'async')
+                if clock_prop : # 時計が表示されていたら消す
+                    clock_prop.kill()
+                    clock_prop = ''
+                time.sleep(330)
+                before_sw = sw
+        else : # ものが置かれていない
+            time.sleep(5)
+            sw = GPIO.input(gpio_sw)
+            if sw == 1 : # ものが置かれていない
+                chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk --incognito http://localhost:1880/clock'
+                clock_prop = run_cmd_func(chromium_start_cmd, 'async')
+                if video_prop : # ビデオチャットが表示されていたら消す
+                    video_prop.kill()
+                    video_prop = ''
+                before_sw = sw
+
+
