2032eef4da710ae6d95532e575e6af1f869531ec
RyogaTakao <170441089@ccalumni.meijo-u.ac.jp>
2020-11-12 13:54:59 +0900

    音声認識完成！

--- src/makeConversationLevel.py
+++ src/makeConversationLevel.py
@@ -1,11 +1,16 @@
+import os
 import socket
 import subprocess
 import time
 
+# ソースファイルの場所取得
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # juliusローカルサーバの立ち上げ
 subprocess.run(['sudo', 'modprobe', 'snd-pcm-oss'])
-proc_julius = subprocess.Popen(['julius', '-C', 'main.jconf', '-C', 'am-dnn.jconf', '-module', '-charconv', 'utf-8', 'sjis', '-dnnconf', 'julius.dnnconf'])
-time.sleep(10)
+proc_julius = subprocess.Popen(['julius', '-C', '/home/pi/dictation-kit-v4.3.1-linux/main.jconf', '-C', '/home/pi/dictation-kit-v4.3.1-linux/am-gmm.jconf', '-module'])
+# proc_julius = subprocess.Popen(['julius', '-C', 'main.jconf', '-C', 'am-dnn.jconf', '-module', '-charconv', 'utf-8', 'sjis', '-dnnconf', 'julius.dnnconf'])
+time.sleep(5)
 
 # Juliusにソケット通信で接続
 client_julius = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,8 +36,19 @@ try:
             data += str(client_julius.recv(1024).decode('utf-8'))
             print('NotFound')
 except KeyboardInterrupt:
+    talk_status = 0
     print('finished')
     print(arry_word)
     client_julius.send("DIE".encode('utf-8'))
     client_julius.close()
     proc_julius.kill()
+    if len(arry_word) > 10:
+        talk_status = 2
+    elif 5 < len(arry_word) < 10:
+        talk_status = 1
+    # トークステータスをテキストファイルに保存
+    with open(APP_ROOT+'/talkStatus.txt', 'w') as f:
+        print(talk_status, file=f)
+        print('', file=f)
+        for word in arry_word:
+            print(word, file=f)
