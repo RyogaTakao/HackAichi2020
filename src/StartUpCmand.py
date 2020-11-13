d00c3e6c1b271c3b84f109c2956a74f93817acce
TatsuyaOkazaki <180442029@ccmailg.meijo-u.ac.jp>
2020-11-13 17:42:58 +0900

    ラズパイ側の処理が完成した

--- src/StartUpCmand.py
+++ src/StartUpCmand.py
@@ -13,29 +13,10 @@ GPIO.setmode(GPIO.BCM)
 # スイッチピンを入力、プルアップに設定
 GPIO.setup(gpio_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# コマンド実行関数
-# async :非期処理 sync :同期処理
-def run_cmd_func (cmd , status) :
-    
-    run_cmd = cmd.split()
-    if status == 'sync' :
-        subprocess.run(run_cmd)
-    elif status == 'async':
-        log = subprocess.Popen(run_cmd)
-        return log
-    else:
-        print('status error decide sync or async')
-
-# 実行フローはここから========================================
-
 # Node-REDの起動コマンドを実行（同期処理）
 nodered_start_cmd = 'node-red-pi --max-old-space-size=256'
-run_cmd_func(nodered_start_cmd, 'async')
+nodered_prop = subprocess.Popen("exec " + nodered_start_cmd, shell=True)
 time.sleep(9)
-
-# 背景をkioskモードで表示するコマンドを実行
-chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk https://localhost:1880/home'
-run_cmd_func(chromium_start_cmd, 'async')
 
 # 永久ループでGPIOをの状態をチェックする
 while True :
@@ -43,21 +24,17 @@ while True :
     sw_1 = GPIO.input(gpio_sw)
     # 物が置いてなかったら
     if sw_1 == 1 :
-        chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk https://localhost:1880/clock'
-        run_cmd_func(chromium_start_cmd, 'async')
-        print('clock')
+        chromium_cmd = 'chromium-browser --noerrdialogs --kiosk http://localhost:1880/clock'
+        clock_prop = subprocess.Popen("exec " + chromium_cmd, shell=True)
+        time.sleep(0.1)
         while True :
             time.sleep(0.1)
             # スイッチの状態を取得
             sw_2 = GPIO.input(gpio_sw)
             if sw_2 == 0 :
-                chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk https://localhost:1880/home'
-                run_cmd_func(chromium_start_cmd, 'async')
-                print('home')
+                clock_prop.kill()
+                time.sleep(0.1)
                 break
-            
-        
-    
     elif sw_1 == 0:
         while True :
             time.sleep(0.1)
@@ -73,18 +50,15 @@ while True :
                         with open(APP_ROOT+'/talkStatus.txt', 'r') as f:
                             status = f.readline()
                     
-                    chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk https://localhost:1880/video-chat?status=' + status 
-                    run_cmd_func(chromium_start_cmd, 'async')
-                    chromium_start_cmd = 'python3 /home/pi/HackAichi2020/src/makeConversationLevel.py'
-                    prop = run_cmd_func(chromium_start_cmd, 'async')
-                    print('video')
-                    for i in range(0, 300):
+                    chromium_cmd = 'chromium-browser --noerrdialogs --kiosk https://meijoryoga.mybluemix.net/video-chat?status=' + status 
+                    videochat_prop = subprocess.Popen("exec " + chromium_cmd, shell=True)
+                    python_cmd = 'python3 /home/pi/HackAichi2020/src/makeConversationLevel.py'
+                    python_prop = subprocess.Popen("exec " + python_cmd, shell=True)
+                    for i in range(0, 20):
                         sw_4 = GPIO.input(gpio_sw)
                         if sw_4 == 0 :
-                            chromium_start_cmd = 'chromium-browser --noerrdialogs --kiosk https://localhost:1880/home'
-                            run_cmd_func(chromium_start_cmd, 'async')
                             break
                         else:
                             time.sleep(1)
-                    prop.kill()
+                    videochat_prop.kill()
                     break
--- src/makeConversationLevel.py
+++ src/makeConversationLevel.py
@@ -2,6 +2,14 @@ import os
 import socket
 import subprocess
 import time
+import RPi.GPIO as GPIO # RPi.GPIOモジュールを使用
+
+# GPIOの使用するピン番号
+gpio_sw = 21
+# GPIO番号指定の準備
+GPIO.setmode(GPIO.BCM)
+# スイッチピンを入力、プルアップに設定
+GPIO.setup(gpio_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # ソースファイルの場所取得
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -17,37 +25,42 @@ client_julius.connect(('localhost', 10500))
 
 arry_word = []
 
-try:
-    data = ""
-    while True:
-        if '</RECOGOUT>\n.' in data:
-            # 出力結果から認識した単語を取り出す
-            recog_text = ""
-            for line in data.split('\n'):
-                index = line.find('WORD="')
-                if index != -1:
-                    line = line[index+6:line.find('"', index+6)]
-                    recog_text = recog_text + line
-            print("認識結果: " + recog_text)
-            arry_word.append(recog_text)
-            data =""
-        else:
-            data += str(client_julius.recv(1024).decode('utf-8'))
-            print('NotFound')
-except KeyboardInterrupt:
-    talk_status = 0
-    print('finished')
-    print(arry_word)
-    client_julius.send("DIE".encode('utf-8'))
-    client_julius.close()
-    proc_julius.kill()
-    if len(arry_word) > 10:
-        talk_status = 2
-    elif 5 < len(arry_word) < 10:
-        talk_status = 1
-    # トークステータスをテキストファイルに保存
-    with open(APP_ROOT+'/talkStatus.txt', 'w') as f:
-        print(talk_status, file=f)
-        print('', file=f)
-        for word in arry_word:
-            print(word, file=f)
+data = ""
+
+for i in range(0, 20):
+    sw_4 = GPIO.input(gpio_sw)
+    if sw_4 == 0 :
+        break
+    else:
+        time.sleep(1)
+    if '</RECOGOUT>\n.' in data:
+        # 出力結果から認識した単語を取り出す
+        recog_text = ""
+        for line in data.split('\n'):
+            index = line.find('WORD="')
+            if index != -1:
+                line = line[index+6:line.find('"', index+6)]
+                recog_text = recog_text + line
+        print("認識結果: " + recog_text)
+        arry_word.append(recog_text)
+        data =""
+    else:
+        data += str(client_julius.recv(1024).decode('utf-8'))
+        print('NotFound')
+
+talk_status = 0
+print('finished')
+print(arry_word)
+client_julius.send("DIE".encode('utf-8'))
+client_julius.close()
+proc_julius.kill()
+if len(arry_word) > 10:
+    talk_status = 2
+elif 5 < len(arry_word) < 10:
+    talk_status = 1
+# トークステータスをテキストファイルに保存
+with open(APP_ROOT+'/talkStatus.txt', 'w') as f:
+    print(talk_status, file=f)
+    print('', file=f)
+    for word in arry_word:
+        print(word, file=f)
